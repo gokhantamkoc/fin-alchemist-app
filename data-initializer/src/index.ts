d919c859f4a47fa4cb68493bde6da4092302f73d
Gökhan Tamkoç <gokhantamkoc@gmail.com>
2025-12-23 08:38:05 +0300

    updated data initializer with api export feature. added some initial data.

--- data-initializer/src/index.ts
+++ data-initializer/src/index.ts
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 
 app.post("/api/export", async (req: Request, res: Response) => {
   const rawData: object = req.body.data;
